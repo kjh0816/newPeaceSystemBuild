8e728fd3da417a43248847c1182172214247e11e
kangdoohyun <rkdengus1208@gmail.com>
2021-08-10 21:10:52 +0900

    NeedLogoutInterceptor 인터셉터 구현

---
+++ src/main/kotlin/com/base/newPeaceSystemBuild/interceptor/NeedLogoutInterceptor.kt
@@ -0,0 +1,26 @@
+package com.base.newPeaceSystemBuild.interceptor
+
+import com.base.newPeaceSystemBuild.vo.Rq
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class NeedLogoutInterceptor : HandlerInterceptor {
+    @Autowired
+    private lateinit var rq: Rq;
+
+    override fun preHandle(req: HttpServletRequest, resp: HttpServletResponse, handler: Any): Boolean {
+        println("로그아웃 인터셉터 실행")
+        if (rq.isLogined()) {
+            rq.respUtf8()
+            rq.printReplaceJs("로그아웃 후 이용해주세요.", "../../usr/account/home")
+
+            return false
+        }
+
+        return super.preHandle(req, resp, handler)
+    }
+}
--- src/main/kotlin/com/base/newPeaceSystemBuild/vo/MyWebMvcConfigurer.kt
+++ src/main/kotlin/com/base/newPeaceSystemBuild/vo/MyWebMvcConfigurer.kt
@@ -2,6 +2,7 @@ package com.base.newPeaceSystemBuild.vo
 
 import com.base.newPeaceSystemBuild.interceptor.BeforeActionInterceptor
 import com.base.newPeaceSystemBuild.interceptor.NeedLoginInterceptor
+import com.base.newPeaceSystemBuild.interceptor.NeedLogoutInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -9,7 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class MyWebMvcConfigurer(
     private val beforeActionInterceptor: BeforeActionInterceptor,
-    private val needLoginInterceptor: NeedLoginInterceptor
+    private val needLoginInterceptor: NeedLoginInterceptor,
+    private val needLogoutInterceptor: NeedLogoutInterceptor
 ) : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
@@ -19,5 +21,10 @@ class MyWebMvcConfigurer(
             .excludePathPatterns("/error")
         registry.addInterceptor(needLoginInterceptor)
             .addPathPatterns("/usr/account/home")
+        registry.addInterceptor(needLogoutInterceptor)
+            .addPathPatterns("/usr/member/login")
+            .addPathPatterns("/usr/member/doLogin")
+            .addPathPatterns("/usr/member/join")
+            .addPathPatterns("/usr/member/doJoin")
     }
 }
