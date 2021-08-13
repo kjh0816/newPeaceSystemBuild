a5edc97b97fd37701ff1bd8d3f21ba8e12e729e9
kangdoohyun <rkdengus1208@gmail.com>
2021-08-13 19:37:01 +0900

    (1) 메인페이지 생성, (2) 장례지도사 등록페이지 생성

---
+++ src/main/kotlin/com/base/newPeaceSystemBuild/controller/UsrHomeController.kt
@@ -0,0 +1,28 @@
+package com.base.newPeaceSystemBuild.controller
+
+import com.base.newPeaceSystemBuild.service.MemberService
+import com.base.newPeaceSystemBuild.util.Ut
+import com.base.newPeaceSystemBuild.vo.Rq
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import javax.servlet.http.HttpSession
+
+
+@Controller
+class UsrHomeController(
+) {
+    @Autowired
+    private lateinit var rq: Rq;
+
+    // VIEW Mapping 함수 시작
+    @RequestMapping("/usr/home/main")
+    fun showLogin(): String {
+        return "usr/home/main"
+    }
+    // VIEW Mapping 함수 끝
+}
