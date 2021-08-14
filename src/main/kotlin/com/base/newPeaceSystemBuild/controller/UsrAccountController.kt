f0085bf44f4acfa4095e23544c186bbbfc5d5294
kjh0816 <readshot2@gmail.com>
2021-08-14 17:17:54 +0900

    메인페이지 연결

--- src/main/kotlin/com/base/newPeaceSystemBuild/controller/UsrAccountController.kt
+++ src/main/kotlin/com/base/newPeaceSystemBuild/controller/UsrAccountController.kt
@@ -2,18 +2,14 @@ package com.base.newPeaceSystemBuild.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 
 @Controller
 class UsrAccountController {
 
     @RequestMapping("/usr/account/home")
-    @ResponseBody
     fun showHome(): String{
 
-        println("안녕하소")
-        return "안녕하소"
-
+        return "usr/home/main"
     }
 }
