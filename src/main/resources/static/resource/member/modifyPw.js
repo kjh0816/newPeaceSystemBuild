431208d1e5d61ebfc2a17073eb910478ca84c7d6
kjh0816 <readshot2@gmail.com>
2021-09-02 15:08:51 +0900

    비밀번호 수정 시, 정규표현식 적용

--- src/main/resources/static/resource/member/modifyPw.js
+++ src/main/resources/static/resource/member/modifyPw.js
@@ -67,3 +67,68 @@ function MemberModifyPw__submit(form) {
 
 
 }
+
+
+
+$('#loginPw').blur(function(){
+    var loginPw = $("#loginPw").val();
+    var loginPwConfirm = $("#loginPwConfirm").val();
+//  (1) 입력 여부 검사
+    if(loginPw.length == 0){
+        $("#loginPwCheckMsg").html("비밀번호를 입력해주세요.");
+        $("#loginPwCheckMsg").css("color", "red");
+        $("#modifyPw-submit").attr("disabled", true);
+        return;
+    }
+//  (2) 입력값에 대한 정규표현식 적용 (시작)
+    var regex = /^[a-zA-Z0-9]{8,16}$/;
+
+    if(!regex.test(loginPw)){
+        $("#loginPwCheckMsg").html('8~16자의 영대소문자 조합만 가능합니다.');
+        $("#loginPwCheckMsg").css("color", "red");
+        $("#modifyPw-submit").attr("disabled", true);
+        return;
+    }
+
+//  (2) 입력값에 대한 정규표현식 적용 (끝)
+
+        $("#loginPwCheckMsg").html("사용 가능한 비밀번호입니다.");
+        $("#loginPwCheckMsg").css("color", "green");
+        $("#modifyPw-submit").attr("disabled", false);
+        return;
+
+
+});
+
+
+
+
+
+$('#loginPwConfirm').blur(function(){
+    var loginPw = $("#loginPw").val();
+    var loginPwConfirm = $("#loginPwConfirm").val();
+    if(loginPwConfirm.length == 0){
+        $("#loginPwConfirmCheckMsg").html("비밀번호를 한 번 더 입력해주세요.");
+        $("#loginPwConfirmCheckMsg").css("color", "red");
+        $("#modifyPw-submit").attr("disabled", true);
+        return;
+    }
+    var regex1 = /^[a-zA-Z0-9]{8,16}$/;
+    if(!regex1.test(loginPwConfirm)){
+            $("#loginPwConfirmCheckMsg").html("8~16자의 영대소문자 조합만 가능합니다.");
+            $("#loginPwConfirmCheckMsg").css("color", "red");
+            $("#modifyPw-submit").attr("disabled", true);
+            return;
+    }
+        if(loginPw != loginPwConfirm){
+            $("#loginPwConfirmCheckMsg").html("비밀번호가 서로 일치하지 않습니다.");
+            $("#loginPwConfirmCheckMsg").css("color", "red");
+            $("#modifyPw-submit").attr("disabled", true);
+            return;
+        }
+
+            $("#loginPwConfirmCheckMsg").html("비밀번호가 서로 일치합니다.");
+            $("#loginPwConfirmCheckMsg").css("color", "green");
+            $("#modifyPw-submit").attr("disabled", false);
+            return;
+});
