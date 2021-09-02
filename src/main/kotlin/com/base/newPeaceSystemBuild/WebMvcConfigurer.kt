1537133fffea0c7ea748650364866be28d293954
kangdoohyun <rkdengus1208@gmail.com>
2021-09-02 12:35:38 +0900

    내 정보 페이지 수정간에 회원가입시 로그인아이디, 이메일 유효성 체크까지 막아버린거 수정

--- src/main/kotlin/com/base/newPeaceSystemBuild/WebMvcConfigurer.kt
+++ src/main/kotlin/com/base/newPeaceSystemBuild/WebMvcConfigurer.kt
@@ -35,6 +35,8 @@ class WebMvcConfigurer(
             .excludePathPatterns("/usr/member/doLogin")
             .excludePathPatterns("/usr/member/join")
             .excludePathPatterns("/usr/member/doJoin")
+            .excludePathPatterns("/usr/member/loginIdCheck")
+            .excludePathPatterns("/usr/member/emailCheck")
         registry.addInterceptor(needAdminInterceptor)
             //          화이트 리스트 방식
             .addPathPatterns("/adm/**")
