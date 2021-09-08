3cc4c78445427028e4480bf5ce0ae3abedb5e2a3
kangdoohyun <rkdengus1208@gmail.com>
2021-09-08 12:41:13 +0900

    관리자 페이지에서 로그아웃이 작동하지 않는 버그 수정

--- src/main/resources/static/resource/member/logout.js
+++ src/main/resources/static/resource/member/logout.js
@@ -1,12 +1,11 @@
 // button을 매개인자로 받지 않으면, 이 함수와 해당 태그가 서로 인식하지 못한다.
 function MemberLogout__submit(button) {
     $.ajax({
-                url: '../member/doLogout',
-                success: function(){
-                    alert('로그아웃되었습니다.');
-//                  로그인 페이지를 절대경로로 이동
-                    window.location.replace('/usr/home/main');
-                }
+        url: '/usr/member/doLogout',
+        success: function(){
+            alert('로그아웃되었습니다.');
+            window.location.replace('/usr/home/main');
+        }
 
-            });
+    });
 }
