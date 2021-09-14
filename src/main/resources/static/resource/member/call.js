296724b5028dad55f59a5aef7d32eb61f673bc3c
kangdoohyun <rkdengus1208@gmail.com>
2021-09-14 21:13:16 +0900

    (1) 영업자 call 페이지 디자인, (2) 영업자 call 페이지 정규 표현식검사

--- src/main/resources/static/resource/member/call.js
+++ src/main/resources/static/resource/member/call.js
@@ -86,3 +86,75 @@ function MemberCall__submit(form) {
     );
 
 }
+
+$('#deceasedName').blur(function(){
+    var deceasedName = $("#deceasedName").val();
+//  (1) 입력 여부 검사
+    if(deceasedName.length == 0){
+        $("#deceasedNameCheckMsg").html("성함을 입력해주세요.");
+        $("#deceasedNameCheckMsg").css("color", "red");
+        $("#call-submit").attr("disabled", true);
+        return;
+    }
+//  (2) 입력값에 대한 정규표현식 적용 (시작)
+    var regex = /^[가-힣]{2,17}$/;
+    if(!regex.test(deceasedName)){
+        $("#deceasedNameCheckMsg").html('한글만 입력해주세요.');
+        $("#deceasedNameCheckMsg").css("color", "red");
+        $("#call-submit").attr("disabled", true);
+        return;
+    }
+//  (2) 입력값에 대한 정규표현식 적용 (끝)
+        $("#deceasedNameCheckMsg").html("");
+        $("#deceasedNameCheckMsg").css("color", "green");
+        $("#call-submit").attr("disabled", false);
+        return;
+});
+
+$('#relatedName').blur(function(){
+    var deceasedName = $("#relatedName").val();
+//  (1) 입력 여부 검사
+    if(deceasedName.length == 0){
+        $("#relatedNameCheckMsg").html("성함을 입력해주세요.");
+        $("#relatedNameCheckMsg").css("color", "red");
+        $("#call-submit").attr("disabled", true);
+        return;
+    }
+//  (2) 입력값에 대한 정규표현식 적용 (시작)
+    var regex = /^[가-힣]{2,17}$/;
+    if(!regex.test(deceasedName)){
+        $("#relatedNameCheckMsg").html('한글만 입력해주세요.');
+        $("#relatedNameCheckMsg").css("color", "red");
+        $("#call-submit").attr("disabled", true);
+        return;
+    }
+//  (2) 입력값에 대한 정규표현식 적용 (끝)
+        $("#relatedNameCheckMsg").html("");
+        $("#relatedNameCheckMsg").css("color", "green");
+        $("#call-submit").attr("disabled", false);
+        return;
+});
+
+$('#cellphoneNo').blur(function(){
+    var cellphoneNo = $("#cellphoneNo").val();
+//  (1) 입력 여부 검사
+    if(cellphoneNo.length == 0){
+        $("#cellphoneNoCheckMsg").html("핸드폰 번호를 입력해주세요.");
+        $("#cellphoneNoCheckMsg").css("color", "red");
+        $("#join-submit").attr("disabled", true);
+        return;
+    }
+//  (2) 입력값에 대한 정규표현식 적용 (시작)
+    var regex = /^[0-9]{9,13}$/;
+    if(!regex.test(cellphoneNo)){
+        $("#cellphoneNoCheckMsg").html('핸드폰 번호가 올바른지 확인해주세요.');
+        $("#cellphoneNoCheckMsg").css("color", "red");
+        $("#join-submit").attr("disabled", true);
+        return;
+    }
+//  (2) 입력값에 대한 정규표현식 적용 (끝)
+        $("#cellphoneNoCheckMsg").html("");
+        $("#cellphoneNoCheckMsg").css("color", "green");
+        $("#join-submit").attr("disabled", false);
+        return;
+});
