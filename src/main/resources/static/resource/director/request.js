de25044701c4628eab7270d1d38e27bd3eecf94b
kangdoohyun <rkdengus1208@gmail.com>
2021-09-14 19:55:09 +0900

    장례지도사 request 페이지 디자인

--- src/main/resources/static/resource/director/request.js
+++ src/main/resources/static/resource/director/request.js
@@ -44,3 +44,15 @@ function DirectorRequest__submit(form) {
     DirectorRequest__submitDone = true;
 }
 // 폼체크 함수 끝
+
+var fileTarget = $('.file-box .upload-hidden');
+
+fileTarget.on('change', function(){ // 값이 변경되면
+    if(window.FileReader){ // modern browser
+        var filename = $(this)[0].files[0].name;
+    }
+    else { // old IE
+        var filename = $(this).val().split('/').pop().split('\\').pop(); // 파일명만 추출
+    } // 추출한 파일명 삽입
+    $(this).siblings('.upload-name').val(filename);
+});
